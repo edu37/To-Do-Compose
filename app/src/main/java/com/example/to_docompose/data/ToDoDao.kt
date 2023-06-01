b985b4112bd3bd7b9125801f04f3fba3c5a9535d
edu37 <dudubezerra33@hotmail.com>
2023-05-31 22:15:27 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/to_docompose/data/ToDoDao.kt
@@ -0,0 +1,39 @@
+package com.example.to_docompose.data
+
+import androidx.room.*
+import com.example.to_docompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTasks(): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table WHERE id=:taskId")
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+    @Query("SELECT * FROM todo_table WHERE description LIKE :searchQuery OR title LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE '$LOW%' THEN 1 WHEN priority LIKE '$MEDIUM%' THEN 2 WHEN priority LIKE '$HIGH%' THEN 3 END")
+    fun sortByLowPriority(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE '$HIGH%' THEN 1 WHEN priority LIKE '$MEDIUM%' THEN 2 WHEN priority LIKE '$LOW%' THEN 3 END")
+    fun sortByHighPriority(searchQuery: String): Flow<List<ToDoTask>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addTask(toDoTask: ToDoTask)
+
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    @Delete
+    suspend fun deleteTask(taskId: Int)
+
+    private companion object {
+        const val LOW = 'L'
+        const val MEDIUM = 'M'
+        const val HIGH = 'H'
+    }
+}
---
+++ app/src/main/java/com/example/to_docompose/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.to_docompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val LowPriorityColor = Color(0xFF77D55C)
+val MediumPriorityColor = Color(0xFFE6772D)
+val HighPriorityColor = Color(0xFFEB4C46)
+val NonePriorityColor = Color(0xFFFFFFFF)
