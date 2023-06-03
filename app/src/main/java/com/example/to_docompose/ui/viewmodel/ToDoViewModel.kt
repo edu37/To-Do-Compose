c19992595a8af25f8383cad72988e22a52c689d4
edu37 <dudubezerra33@hotmail.com>
2023-06-02 22:35:57 -0300

    criação de SharedViewModel

---
+++ app/src/main/java/com/example/to_docompose/ui/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.to_docompose.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.to_docompose.data.models.ToDoTask
+import com.example.to_docompose.domain.repository.ToDoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ToDoViewModel @Inject constructor(
+    private val repository: ToDoRepository
+) : ViewModel() {
+
+    private val _selectedTask = MutableStateFlow<ToDoTask>()
+    val selectedTask: StateFlow<ToDoTask> = _selectedTask
+
+    fun getSelectedTask(taskId: Int) {
+        viewModelScope.launch {
+            repository.getSelectedTask(taskId = taskId).collect { task ->
+                _selectedTask.value = task
+            }
+        }
+    }
+}
