8f633f79fe568886434cd0419e6a27b9b3371928
edu37 <dudubezerra33@hotmail.com>
2023-06-03 23:06:52 -0300

    Implementando Compose Navigation

---
+++ app/src/main/java/com/example/to_docompose/ui/navigation/SetupNavitation.kt
@@ -0,0 +1,25 @@
+package com.example.to_docompose.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.example.to_docompose.util.Constants.LIST_SCREEN
+
+@Composable
+fun SetupNavitation(
+    navControler: NavHostController
+) {
+
+    val screen = remember(navControler) { Navigations(navControler) }
+
+    NavHost(navController = navControler, startDestination = LIST_SCREEN) {
+        listComposable(
+            navigateToTaskScreen = screen.taskScreen
+        )
+        taskComposable(
+            navigateToListScreen = screen.listScreen
+        )
+    }
+
+}
